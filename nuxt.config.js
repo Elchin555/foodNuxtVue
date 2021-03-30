991da1679839378a53a7b1ce6043ac3b76a98d21
elcin1040 <ebil.rehimov@mail.ru>
2021-03-30 18:41:22 +0400

    5

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,44 +1,47 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  target: "static",
+  
   mode: "universal",
+  
+  target: "static",
+  
   head: {
-    title: 'foodapp2',
-    htmlAttrs: {
-      lang: 'en'
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+    title: "foodapp2",
+    meta: [{
+        charset: "utf-8"
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
+      },
+      {
+        hid: "description",
+        name: "description",
+        content: "Demo of Nuxt and Vue"
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-  { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Mulish:wght@300&family=Poppins:wght@600&display=swap' }
+    link: [{
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon.ico"
+      },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Mulish:wght@300&family=Poppins:wght@600&display=swap"
+      }
     ]
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-     `~/assets/main.scss`
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ["~/plugins/getfood.server.js","~/plugins/vuelidate.js"],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
+  /*
+   ** Global CSS
+   */
+  css: ["~/assets/main.scss"],
+  
+  plugins: ["~/plugins/getfood.server.js", "~/plugins/vuelidate.js"],
+  
   components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  
+  buildModules: [],
+  
+  modules: [],
+  
+  build: {}
+};
